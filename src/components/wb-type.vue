7a7fa3bcd58be520a14038a8ea664169b10d3977
Velcro1 <velcro74@gmail.com>
2021-06-02 11:56:22 -0400

    Added select element to types

--- src/components/wb-type.vue
+++ src/components/wb-type.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="wb-container col-md-5">
         <label>WARBAND TYPE:</label>
-        <input />
+        <b-form-select v-model="selected" v-bind:options="options"></b-form-select>
     </div>
 </template>
 
 <script>
 export default {
   name: 'wb-type',
+  data() {
+      return {
+        selected: null,
+        options: [
+          { value: null, text: '' },
+          { value: 'undead', text: 'Undead' },
+          { value: 'skaven', text: 'Skaven' },
+          { value: 'sisters-of-sigmar', text: 'Sisters of Sigmar' },
+          { value: 'witch-hunters', text: 'Witch Hunters' },
+        ]
+      }
+  }
 }
 </script>
