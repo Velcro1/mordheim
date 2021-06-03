6a1a9ad3e0f9fc9c3abfcc0d941ff1a8ff9e68b9
Velcro1 <velcro74@gmail.com>
2021-06-03 16:42:36 -0400

    Created Hero Card

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,43 @@
 <template>
-  <div id="app" class="container">
+  <b-container id="app">
     <h1 class="h-title">{{userSelect}}</h1>
-    <div class="row">
-      <div class="wb-container col-md-6">
+    <b-row align-h="between">
+      <b-col class="wb-container" cols="7">
         <label>WARBAND NAME:</label>
         <input type="text" v-model="title">
-    </div>
-      <div class="wb-container col-md-5">
+    </b-col>
+      <b-col class="wb-container" cols="4">
         <label>WARBAND TYPE:</label>
         <b-form-select v-model="userSelect" :options="warBands.warbands" value-field="name" text-field="name" @change="startCharacter"></b-form-select>
-    </div>
-    </div>
-    <div class="row">
-         <div class="wb-container col-md-3">
-      <label>TREASURY:</label>
-      <p>Gold Crowns:{{ startGold }}</p>
-      <p>Wyrdstone Shards:</p>
-      <input/>
-    </div>
-         <div class="wb-container col-md-3">
+    </b-col>
+    </b-row>
+    <b-row>
+      <b-col class="wb-container" cols="3">
+        <label>TREASURY:</label>
+        <p>Gold Crowns: {{ startGold }}</p>
+        <p>Wyrdstone Shards:</p>
+        <input/>
+      </b-col>
+         <b-col class="wb-container" cols="3">
       <label>WARBAND RATING:</label>
       <p>Total Experience: <input /></p>
       <p>Memebers ( ) x 5: <input /></p>
       <p>Rating: <input /></p>
-    </div>
-      <div class="wb-container col-md-5">
+    </b-col>
+      <b-col class="wb-container" cols="6">
       <label>STORED EQUIPMENT:</label>
-      <textarea />
-    </div>
-    </div>
-  </div>
+    </b-col>
+    </b-row>
+    <b-row v-if="userSelect">
+      <h2 class="h-title">{{heroType}}</h2>
+    </b-row>
+    <HeroCard v-if="userSelect"/>
+  </b-container>
 </template>
 
 <script>
 import warBands from './data/warbands.json';
+import HeroCard from './components/hero-card.vue';
 
 
 export default {
@@ -44,10 +48,11 @@ export default {
       userSelect: '',
       warBands,
       startGold: 0,
+      heroType: 'Heroes',
     }
   },
   components: {
-
+    HeroCard,
   },
   methods: {
     startCharacter() {
---
+++ src/components/hero-card.vue
@@ -0,0 +1,51 @@
+<template>
+<b-row>
+    <b-col class="wb-container">
+        <div class="grid-container">
+            <div class="Cell-4">Experience</div>
+            <div class="Cell-1">
+                <div class="row-1">Name</div>
+                <div class="row-2">Type</div>
+                <div class="row-3">Stats</div>
+            </div>
+            <div class="Cell-2">EQUIPMENT</div>
+            <div class="Cell-3">Sklls</div>
+        </div>
+    </b-col>
+</b-row>
+</template>
+
+<script>
+    export default {
+        name: 'HeroCard',
+    }
+</script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    "Cell-1 Cell-2 Cell-3"
+    "Cell-4 Cell-4 Cell-4";
+}
+.Cell-4 { grid-area: Cell-4; }
+.Cell-1 {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    "row-1"
+    "row-2"
+    "row-3";
+  grid-area: Cell-1;
+}
+.row-1 { grid-area: row-1; }
+.row-2 { grid-area: row-2; }
+.row-3 { grid-area: row-3; }
+.Cell-2 { grid-area: Cell-2; }
+.Cell-3 { grid-area: Cell-3; }
+</style>
