a58f3b614377658d3190608acc2f1550f8f732b9
Velcro1 <velcro74@gmail.com>
2021-06-04 16:01:21 -0400

    Finished Leader Card

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,36 @@
 <template>
   <b-container id="app">
-    <h1 class="h-title">{{userSelect}}</h1>
+    <h1 class="h-title">{{wbType ? wbType : placeHolder}}</h1>
     <b-row align-h="between">
-      <b-col class="wb-container" cols="7">
+      <b-col class="wb-container wb-name mb-3" cols="12" md="7">
         <label>WARBAND NAME:</label>
-        <input type="text" v-model="title">
+        <input type="text" v-model="wbName">
     </b-col>
-      <b-col class="wb-container" cols="4">
+      <b-col class="wb-container wb-type mb-3" cols="12" md="4">
         <label>WARBAND TYPE:</label>
-        <b-form-select v-model="userSelect" :options="warBands.warbands" value-field="name" text-field="name" @change="startCharacter"></b-form-select>
+        <b-form-select v-model="wbType" :options="warBands.warbands" value-field="name" text-field="name" @change="startCharacter"></b-form-select>
     </b-col>
     </b-row>
-    <b-row>
-      <b-col class="wb-container" cols="3">
+    <b-row align-h="between">
+      <b-col class="wb-container wb-treasury mb-3" cols="12" md="3">
         <label>TREASURY:</label>
         <p>Gold Crowns: {{ startGold }}</p>
         <p>Wyrdstone Shards:</p>
-        <input/>
       </b-col>
-         <b-col class="wb-container" cols="3">
-      <label>WARBAND RATING:</label>
-      <p>Total Experience: <input /></p>
-      <p>Memebers ( ) x 5: <input /></p>
-      <p>Rating: <input /></p>
+      <b-col class="wb-container wb-rating mb-3" cols="12" md="3">
+        <label>WARBAND RATING:</label>
+        <p>Total Experience: {{ totalExp }}</p>
+        <p>Members ( {{ totalMembers }} ) x 5: {{ membersRating }}</p>
+        <p>Rating: {{ totalExp + membersRating }}</p>
     </b-col>
-      <b-col class="wb-container" cols="6">
+      <b-col class="wb-container wb-equip mb-3" cols="12" md="6">
       <label>STORED EQUIPMENT:</label>
     </b-col>
     </b-row>
-    <b-row v-if="userSelect">
-      <h2 class="h-title">{{heroType}}</h2>
+    <b-row v-if="wbType">
+      <h2 class="h-title">{{heroesTitle}}</h2>
     </b-row>
-    <HeroCard v-if="userSelect"/>
+    <HeroCard v-if="leader" :hero="hero"/>
   </b-container>
 </template>
 
@@ -44,29 +43,66 @@ export default {
   name: 'App',
   data() {
     return {
-      title: '',
-      userSelect: '',
       warBands,
+      wbName: '',
+      wbType: '',
+      heroType: '',
+      placeHolder: 'Warband Name',
+      heroesTitle: 'Heroes',
+      henchmanTitle: 'Henchmen',
       startGold: 0,
-      heroType: 'Heroes',
+      totalMembers: 0,
+      membersRating: 0,
+      totalExp: 0,
+      hero: '',
+      leader: false,
     }
   },
   components: {
     HeroCard,
   },
+  computed: {
+  },
   methods: {
     startCharacter() {
       let data = this.warBands.warbands;
       data.filter(item => {
-        if ( item.name === this.userSelect) {
-          this.startGold = item.startGold;
+        if ( item.name === this.wbType) {
+          this.startGold = item.startGold - item.leader.cost;
+          this.leader = true;
+          this.totalMembers++;
+          this.membersRating = this.totalMembers * 5;
+          this.hero = item.leader;
+          this.totalExp = item.leader.startExp;
         }
       })
-    }
+      console.log(this.warBands.warbands[6].heroes);
+    },
+    addHero() {
+      let data = this.warBands.warbands;
+      data.filter(item => {
+        if ( item.name === this.wbType) {
+         console.log("jhfd");
+        }
+      })
+    },
   }
 }
 </script>
 
 <style lang="scss">
 @import '@/design/index.scss';
+
+@media (min-width: 768px){
+  .wb-name.col-md-7 {
+    width: 60%;
+  }
+  .wb-type.col-md-4{
+    width: 38%;
+  }
+  .wb-treasury.col-md-3,
+  .wb-rating.col-md-3{
+    width: 23%;
+  }
+}
 </style>
--- src/components/hero-card.vue
+++ src/components/hero-card.vue
@@ -1,51 +1,241 @@
 <template>
 <b-row>
-    <b-col class="wb-container">
-        <div class="grid-container">
-            <div class="Cell-4">Experience</div>
-            <div class="Cell-1">
-                <div class="row-1">Name</div>
-                <div class="row-2">Type</div>
-                <div class="row-3">Stats</div>
-            </div>
-            <div class="Cell-2">EQUIPMENT</div>
-            <div class="Cell-3">Sklls</div>
-        </div>
+    <b-col class="wb-container" cols="12">
+        <b-row>
+            <b-col cols="4">
+               <b-row>
+                    <b-col id="hero-name" class="hero-name p-2" cols="12" md="12">NAME</b-col>
+                </b-row>
+                <b-row>
+                    <b-col class="hero-type p-2" cols="12" md="12">TYPE <span>{{ hero.type }}</span></b-col>
+                </b-row>
+                <b-row>
+                    <b-col class="hero-stats p-2" cols="12" md="12">
+                        <table class="stats">
+                            <thead>
+                            <tr>
+                                <th>M</th>
+                                <th>WS</th>
+                                <th>BS</th>
+                                <th>S</th>
+                                <th>T</th>
+                                <th>W</th>
+                                <th>I</th>
+                                <th>A</th>
+                                <th>Ld</th>
+                            </tr>
+                            </thead>
+                        <tbody>
+                            <tr>
+                                <td>{{ hero.stats.m }}</td>
+                                <td>{{ hero.stats.ws }}</td>
+                                <td>{{ hero.stats.bs }}</td>
+                                <td>{{ hero.stats.s }}</td>
+                                <td>{{ hero.stats.t }}</td>
+                                <td>{{ hero.stats.w }}</td>
+                                <td>{{ hero.stats.i }}</td>
+                                <td>{{ hero.stats.a }}</td>
+                                <td>{{ hero.stats.ld }}</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </b-col>
+               </b-row>
+            </b-col>
+            <b-col class="hero-equip p-2" cols="4">EQUIPMENT</b-col>
+            <b-col class="hero-skills p-2" cols="4">SKILLS, INJURIES, ETC</b-col>
+        </b-row>
+
+
+        <b-row>
+            <b-col class="hero-exp p-2 pt-3" cols="12">
+                <h5>Experience:</h5>
+                <table class="exp" id="mytab1">
+                <tbody>
+                <tr>
+                <td><div id="expX-0" class="exp-block"></div></td>
+                <td><div id="expX-1" class="exp-block expX-1 double"></div></td>
+                <td><div id="expX-2" class="exp-block expX-2"></div></td>
+                <td><div id="expX-3" class="exp-block expX-3 double"></div></td>
+                <td><div id="expX-4" class="exp-block expX-4"></div></td>
+                <td><div id="expX-5" class="exp-block double"></div></td>
+                <td><div id="expX-6" class="exp-block"></div></td>
+                <td><div id="expX-7" class="exp-block double"></div></td>
+                <td><div id="expX-8" class="exp-block"></div></td>
+                <td><div id="expX-9" class="exp-block"></div></td>
+                <td><div id="expX-10" class="exp-block double"></div></td>
+                <td><div id="expX-11" class="exp-block"></div></td>
+                <td><div id="expX-12" class="exp-block"></div></td>
+                <td><div id="expX-13" class="exp-block double"></div></td>
+                <td><div id="expX-14" class="exp-block"></div></td>
+                <td><div id="expX-15" class="exp-block"></div></td>
+                <td><div id="expX-16" class="exp-block double"></div></td>
+                <td><div id="expX-17" class="exp-block"></div></td>
+                <td><div id="expX-18" class="exp-block"></div></td>
+                <td><div id="expX-19" class="exp-block double"></div></td>
+                <td><div id="expX-20" class="exp-block"></div></td>
+                <td><div id="expX-21" class="exp-block"></div></td>
+                <td><div id="expX-22" class="exp-block"></div></td>
+                <td><div id="expX-23" class="exp-block double"></div></td>
+                <td><div id="expX-24" class="exp-block"></div></td>
+                <td><div id="expX-25" class="exp-block"></div></td>
+                <td><div id="expX-26" class="exp-block"></div></td>
+                <td><div id="expX-27" class="exp-block double"></div></td>
+                <td><div id="expX-28" class="exp-block"></div></td>
+                <td><div id="expX-29" class="exp-block"></div></td>
+                </tr>
+                <tr>
+                <td><div id="expX-30" class="exp-block"></div></td>
+                <td><div id="expX-31" class="exp-block double"></div></td>
+                <td><div id="expX-32" class="exp-block"></div></td>
+                <td><div id="expX-33" class="exp-block"></div></td>
+                <td><div id="expX-34" class="exp-block"></div></td>
+                <td><div id="expX-35" class="exp-block double"></div></td>
+                <td><div id="expX-36" class="exp-block"></div></td>
+                <td><div id="expX-37" class="exp-block"></div></td>
+                <td><div id="expX-38" class="exp-block"></div></td>
+                <td><div id="expX-39" class="exp-block"></div></td>
+                <td><div id="expX-40" class="exp-block double"></div></td>
+                <td><div id="expX-41" class="exp-block"></div></td>
+                <td><div id="expX-42" class="exp-block"></div></td>
+                <td><div id="expX-43" class="exp-block"></div></td>
+                <td><div id="expX-44" class="exp-block"></div></td>
+                <td><div id="expX-45" class="exp-block double"></div></td>
+                <td><div id="expX-46" class="exp-block"></div></td>
+                <td><div id="expX-47" class="exp-block"></div></td>
+                <td><div id="expX-48" class="exp-block"></div></td>
+                <td><div id="expX-49" class="exp-block"></div></td>
+                <td><div id="expX-50" class="exp-block double"></div></td>
+                <td><div id="expX-51" class="exp-block"></div></td>
+                <td><div id="expX-52" class="exp-block"></div></td>
+                <td><div id="expX-53" class="exp-block"></div></td>
+                <td><div id="expX-54" class="exp-block"></div></td>
+                <td><div id="expX-55" class="exp-block"></div></td>
+                <td><div id="expX-56" class="exp-block double"></div></td>
+                <td><div id="expX-57" class="exp-block"></div></td>
+                <td><div id="expX-58" class="exp-block"></div></td>
+                <td><div id="expX-59" class="exp-block"></div></td>
+                </tr>
+                <tr>
+                <td><div id="expX-60" class="exp-block"></div></td>
+                <td><div id="expX-61" class="exp-block"></div></td>
+                <td><div id="expX-62" class="exp-block double"></div></td>
+                <td><div id="expX-63" class="exp-block"></div></td>
+                <td><div id="expX-64" class="exp-block"></div></td>
+                <td><div id="expX-65" class="exp-block"></div></td>
+                <td><div id="expX-66" class="exp-block"></div></td>
+                <td><div id="expX-67" class="exp-block"></div></td>
+                <td><div id="expX-68" class="exp-block double"></div></td>
+                <td><div id="expX-69" class="exp-block"></div></td>
+                <td><div id="expX-70" class="exp-block"></div></td>
+                <td><div id="expX-71" class="exp-block"></div></td>
+                <td><div id="expX-72" class="exp-block"></div></td>
+                <td><div id="expX-73" class="exp-block"></div></td>
+                <td><div id="expX-74" class="exp-block"></div></td>
+                <td><div id="expX-75" class="exp-block double"></div></td>
+                <td><div id="expX-76" class="exp-block"></div></td>
+                <td><div id="expX-77" class="exp-block"></div></td>
+                <td><div id="expX-78" class="exp-block"></div></td>
+                <td><div id="expX-79" class="exp-block"></div></td>
+                <td><div id="expX-80" class="exp-block"></div></td>
+                <td><div id="expX-81" class="exp-block"></div></td>
+                <td><div id="expX-82" class="exp-block double"></div></td>
+                <td><div id="expX-83" class="exp-block"></div></td>
+                <td><div id="expX-84" class="exp-block"></div></td>
+                <td><div id="expX-85" class="exp-block"></div></td>
+                <td><div id="expX-86" class="exp-block"></div></td>
+                <td><div id="expX-87" class="exp-block"></div></td>
+                <td><div id="expX-88" class="exp-block"></div></td>
+                <td><div id="expX-89" class="exp-block double"></div></td>
+                </tr>
+                </tbody>
+                </table>
+            </b-col>
+        </b-row>
     </b-col>
 </b-row>
 </template>
 
 <script>
-    export default {
-        name: 'HeroCard',
-    }
+
+export default {
+    name: 'HeroCard',
+    props: ['hero'],
+    data() {
+        return {
+            expPoint: 'x',
+
+        }
+    },
+    mounted() {
+        for(let i = 0; i < this.hero.startExp; i++){
+           document.getElementById('expX-'+i).innerHTML = "x";
+        }
+    },
+    methods: {
+
+    },
+}
 </script>
 
-<style scoped>
-.grid-container {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  gap: 0px 0px;
-  grid-template-areas:
-    "Cell-1 Cell-2 Cell-3"
-    "Cell-4 Cell-4 Cell-4";
+<style lang="scss" scoped>
+.wb-container {
+    padding-top: 0;
+    padding-bottom: 0;
 }
-.Cell-4 { grid-area: Cell-4; }
-.Cell-1 {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  gap: 0px 0px;
-  grid-template-areas:
-    "row-1"
-    "row-2"
-    "row-3";
-  grid-area: Cell-1;
+.hero-name,
+.hero-type,
+.hero-equip,
+.hero-skills {  
+    border-bottom: 2px solid black;
 }
-.row-1 { grid-area: row-1; }
-.row-2 { grid-area: row-2; }
-.row-3 { grid-area: row-3; }
-.Cell-2 { grid-area: Cell-2; }
-.Cell-3 { grid-area: Cell-3; }
+.hero-equip,
+.hero-skills {
+    border-left: 2px solid black;
+}
+.hero-type {
+    span {
+        font-weight: bold;
+        font-size: 1.2rem;
+        padding-left: 15px;
+    }
+}
+table.stats {
+    th {
+        border: 2px solid black;
+        background-color: black;
+        color: #fff;
+        width: 5%;
+         text-align: center;
+    }
+    td {
+        border: 2px solid black;
+        text-align: center;
+    }
+}
+.hero-exp {
+    .exp {
+        display: inline-block;
+        .exp-block {
+            border: 1px solid black;
+            width: 25px;
+            height: 25px;
+            margin: 0 5px 5px 0;
+            font-size: 40px;
+            line-height: 10px;
+            text-align: center;
+            padding: 3px;
+        }
+        .double {
+            border: 4px solid black;
+            padding: 0;
+        }
+    }
+}
+h5 {
+    display: inline-block;
+    vertical-align: top;
+    margin-right: 15px;
+}
+
 </style>
